9a4ca915a05258d966cf40d59f7e240e2f1bf977
rodrie2 <rodrie2@unlv.nevada.edu>
2016-04-30 23:55:01 -0700

    Added files via upload

---
+++ DA6/DA6T1/NOKIALCD/NOKIALCD/main.c
@@ -0,0 +1,83 @@
+/*
+ * DA6NOKIA.c
+ *
+ * Created: 4/28/2016 8:58:41 AM
+ *  Author: rodrie2
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+#define F_CPU 8000000UL
+#include "nokia5110.c"
+
+//from .h
+/*#define LCD_SCE PB2
+#define LCD_RST PB1
+#define LCD_DC PB6
+#define LCD_DIN PB3
+#define LCD_CLK PB5*/
+#define ACD_0 PC0
+
+int main(void)
+{
+	int adc_temp;
+	char temp[8];
+	DDRB = 0xFF;
+	
+	SPI_MasterInit();
+	
+//from nokia5110.c
+//inserted transmit function in file.	
+ nokia_lcd_init();
+ nokia_lcd_write_string("The current", 1);
+ nokia_lcd_set_cursor(0,10);
+ nokia_lcd_write_string("temperature is", 1);
+ ADC_init();
+ 
+ while(1)
+ {
+	 adc_temp = adc_read();
+	 adc_temp = (adc_temp)/3;
+	 sprintf(temp, "%d", adc_temp);
+	 nokia_lcd_set_cursor(0, 20);
+	 nokia_lcd_write_string(temp, 1);
+	 nokia_lcd_write_string(" degrees", 1);
+	 nokia_lcd_set_cursor(0, 30);
+	 nokia_lcd_write_string("Fahrenheit", 1);
+	 nokia_lcd_render();
+	 //ADCSRA |= (1<<ADIF);
+	 _delay_ms(1000);
+ }
+ return 0;   
+}
+
+int adc_read()
+{
+	ADMUX &= 0x00;
+	ADCSRA |= (1<<ADSC);
+	while((ADCSRA & (1<<ADSC)));
+	return ADC;
+}
+void ADC_init(void)
+{
+	ADCSRA = (1<<ADEN);//|(1<<ADSC);//|(1<<ADIF);
+}
+//ADOPTED FROM ATMEGA328P DATASHEET
+void SPI_MasterInit(void)
+{
+	/* Set MOSI and SCK output, all others input */
+	//ALREADY DONE OUTSIDE FUNCTION
+	//DDR_SPI = (1<<DD_MOSI)|(1<<DD_SCK);
+	/* Enable SPI, Master, set clock rate fck/16 */
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+}
+/*
+//PLACED IN OTHER FUNCTION
+void SPI_MasterTransmit(char cData)
+{
+	/* Start transmission //
+	SPDR = cData;
+	/* Wait for transmission complete /
+	while(!(SPSR & (1<<SPIF)))
+	;
+}*/
---
+++ DA6/DA6T1/NOKIALCD/NOKIALCD/spi_alone.c
@@ -0,0 +1,94 @@
+/*
+ * spi.c
+ *
+ * Created: 4/29/2016 9:41:27 PM
+ * Author : Rodriguez
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+#define F_CPU 8000000UL
+#define DDR_SPI DDRB
+#define DD_MOSI PB3
+#define DD_SCK PB5
+#define LCD_SCE PB2
+#define LCD_DE PB6
+#define LCD_RST PB1
+
+
+int main(void)
+{
+	char data;
+	DDRB = 0xFF;
+	
+	PORTB &= ~(1<<LCD_RST);
+	PORTB |= (1<<LCD_SCE);
+	SPI_MasterInit();
+	PORTB |= (1<<LCD_RST);
+	PORTB &= ~(1<<LCD_SCE);
+	PORTB &= ~(1<<LCD_DE);	
+	
+	SPI_MasterTransmit(0x21);
+	_delay_ms(1000);
+	SPI_MasterTransmit(0x13);
+	_delay_ms(1000);
+	SPI_MasterTransmit(0x06);
+	_delay_ms(1000);
+		SPI_MasterTransmit(0xC2);
+		_delay_ms(1000);
+			SPI_MasterTransmit(0x20);
+			_delay_ms(1000);
+				SPI_MasterTransmit(0x09);
+				_delay_ms(1000);
+					SPI_MasterTransmit(0x80);
+					_delay_ms(1000);
+						SPI_MasterTransmit(0x40);
+						_delay_ms(1000);
+						PORTB |= (1<<LCD_DE);
+						for (int i = 0; i < 504; i++)
+						{
+							SPI_MasterTransmit(0x00);
+						}
+							PORTB &= ~(1<<LCD_DE);
+							SPI_MasterTransmit(0x08);
+							_delay_ms(1000);
+	//start writing PH but in code, need table for ass
+	SPI_MasterTransmit(0x0C);
+	_delay_ms(1000);
+	PORTB |= (1<<LCD_DE);
+	SPI_MasterTransmit(0x1F);
+	_delay_ms(1000);
+	SPI_MasterTransmit(0x05);
+	_delay_ms(1000);
+	SPI_MasterTransmit(0x07);
+	_delay_ms(1000);
+	SPI_MasterTransmit(0x00);
+	_delay_ms(1000);
+		SPI_MasterTransmit(0x1f);
+		_delay_ms(1000);
+			SPI_MasterTransmit(0x04);
+			_delay_ms(1000);
+				SPI_MasterTransmit(0x1f);
+				_delay_ms(1000);
+				SPI_MasterTransmit(0x00);
+	//PORTB &= ~(1<<LCD_DE);
+	//SPI_MasterTransmit(0x00);			
+	while(1){}
+	return 0;
+	}
+
+void SPI_MasterInit(void)
+{
+	/* Set MOSI and SCK output, all others input */
+	//DDR_SPI = (1<<DD_MOSI)|(1<<DD_SCK);
+	/* Enable SPI, Master, set clock rate fck/16 */
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+}
+void SPI_MasterTransmit(char cData)
+{
+	/* Start transmission */
+	SPDR = cData;
+	/* Wait for transmission complete */
+	while(!(SPSR & (1<<SPIF)))
+	;
+}
